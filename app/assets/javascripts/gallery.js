35bc622f23878b5b25c11de83458ce4edf90aaa8
Mainor <m_claros@live.com>
2013-12-03 21:23:53 -0800

    first commit

---
+++ app/assets/javascripts/gallery.js
@@ -0,0 +1,13 @@
+window.Gallery = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Gallery.initialize();
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gallery::Application.config.secret_token = '2cd310d4341299eca4c4416f022c714398859b3133c827e9e58a6dc693910dfe26d18e8cdb35008342ea0b6d54c82a83172e2ef1e10f86a9fed4ff8009764308'
